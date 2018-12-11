8ef166c2cbf32f0c0844fa0821f7a054f5526df3
jembrown <jeremymbrown@gmail.com>
2018-12-11 06:39:39 -0600

    insulinTreatment.py

--- assignments/ewhart1/insulinTreatment.py
+++ assignments/ewhart1/insulinTreatment.py
@@ -3,9 +3,11 @@
 import matplotlib.pyplot as plt
 
 fig = plt.figure()
-fig.suptitle("Blood Glucose Levels Following Insulin Injection in Non-Diabetic Subject", fontsize=18)
+fig.suptitle("Blood Glucose Levels Following Insulin Injection in Non-Diabetic Subject", fontsize=10)
 plt.xlabel("Minutes Post-Injection", fontsize=14)
 plt.ylabel("Glucose (mg/dL)", fontsize=14)
 plt.plot([0,5,10,20,30,40,50,60], [104,98,85,70,57,44,36,51])
 plt.axis([0, 60, 0, 110])
 plt.show()
+
+# DB: Good! I just adjusted the font size of the title to be readable.
